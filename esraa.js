87fd11c7fef05a1d236c34489d40ee1d248d13d6
esra94sufyan <79212781+esra94sufyan@users.noreply.github.com>
2021-09-22 15:30:21 +0300

    Add files via upload

---
+++ esraa.js
@@ -0,0 +1,4 @@
+var i=1;
+for (k=1; k<10; k++){
+    i+=k
+console.log(i) }
---
+++ javatest.js
@@ -0,0 +1,8 @@
+var arr=[1,-2,3,-4,5,-6]
+
+function positive (arr) {
+    const pa = arr.filter(num => num > 0) 
+     
+    console.log(pa)
+}
+
